3dab898fdf209a568831ca1e073f7d66f6b4f242
mmmgdzl <738272815@qq.com>
2018-10-25 13:25:52 +0800

    SSM模板搭建完成

---
+++ src/main/java/com/mmmgdzl/test/Animal.java
@@ -0,0 +1,23 @@
+package com.mmmgdzl.test;
+
+public class Animal {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
